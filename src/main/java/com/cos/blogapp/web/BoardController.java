7cc74e99dc88e5d0798439a86d44194ca5e466bc
ssar <ssar@example.com>
2021-09-13 11:48:52 +0900

    글쓰기 완료

--- src/main/java/com/cos/blogapp/web/BoardController.java
+++ src/main/java/com/cos/blogapp/web/BoardController.java
@@ -1,14 +1,27 @@
 package com.cos.blogapp.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cos.blogapp.domain.board.Board;
 import com.cos.blogapp.domain.board.BoardRepository;
+import com.cos.blogapp.domain.user.User;
+import com.cos.blogapp.util.Script;
+import com.cos.blogapp.web.dto.BoardSaveReqDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +30,35 @@ import lombok.RequiredArgsConstructor;
 public class BoardController {
 	// final을 붙으면 무조건 초기화를 해야함
 	private final BoardRepository boardRepository;
+	private final HttpSession session;
+	
+	@PostMapping("/board")
+	public @ResponseBody String save(@Valid BoardSaveReqDto dto, BindingResult bindingResult) {
+		
+		User principal = (User) session.getAttribute("principal");
+		
+		//인증체크
+		if(principal == null) {// 로그인 안됨
+			return Script.href("/loginForm", "잘못된 접근입니다.");
+		}
+		
+		if(bindingResult.hasErrors()) {			
+			Map<String, String> errorMap = new HashMap<>();
+			for(FieldError error : bindingResult.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			return Script.back(errorMap.toString());
+		}
+		
+		
+		boardRepository.save(dto.toEntity(principal));		
+		return Script.href("/", "글쓰기 완료");
+	}
+	
+	@GetMapping("/board/saveForm")
+	public String saveForm() {
+		return "board/saveForm";
+	}
 	
 	@GetMapping( "/board")
 	public String home(Model model, int page) {//Model == request.setAttribute
