1c64b818ebd341bf7d53d6a0959aa353a459d3a3
ssar <ssar@example.com>
2021-09-16 11:43:50 +0900

    커스텀 익셉션 처리

--- src/main/java/com/cos/blogapp/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cos/blogapp/handler/GlobalExceptionHandler.java
@@ -1,21 +1,20 @@
 package com.cos.blogapp.handler;
 
-import java.util.NoSuchElementException;
-
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cos.blogapp.handler.ex.MyNotFoundException;
 import com.cos.blogapp.util.Script;
 
 // @ControllerAdvice 1. 익셉션 핸들링  2.@Controller역할까지 한다
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
-	@ExceptionHandler(value = NoSuchElementException.class)
-	public @ResponseBody String error1(NoSuchElementException e) {
+	@ExceptionHandler(value = MyNotFoundException.class)
+	public @ResponseBody String error1(MyNotFoundException e) {
 		System.out.println(e.getMessage());
-		return Script.href("/", "게시글 id를 찾을 수 없습니다.");
+		return Script.href("/", e.getMessage());
 	}
 }
 
