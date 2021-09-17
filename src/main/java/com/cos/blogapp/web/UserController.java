039e13ea3ab2a53b9397989065ef2401ef453425
ssar <ssar@example.com>
2021-09-17 09:53:50 +0900

    회원수정페이지 만들기

--- src/main/java/com/cos/blogapp/web/UserController.java
+++ src/main/java/com/cos/blogapp/web/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -29,6 +30,14 @@ import lombok.RequiredArgsConstructor;
 public class UserController {	
 	private final UserRepository userRepository;	
 	private final HttpSession session;
+	
+	@GetMapping("/user/{id}")
+	public String userInfo(@PathVariable int id) {
+		// 기본은 userRepository.findByid(id) 디비에서 가져와야함.
+		// 편법은 세션값을 가져올 수도 있다
+		
+		return "user/updateForm";
+	}
 	
 	@GetMapping("/logout")
 	public String logout() {
